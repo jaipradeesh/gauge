df9c3558b2cf6ccf895b0fc725c9cc3b570a356b
Dharmendra Singh <dharmenn@thoughtworks.com>
2019-09-05 11:29:44 +0530

    WIP

--- execution/stepExecutor.go
+++ execution/stepExecutor.go
@@ -18,12 +18,24 @@
 package execution
 
 import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"image/png"
+	"os"
+	"path/filepath"
+
+	"github.com/getgauge/gauge/config"
+	"github.com/getgauge/gauge/logger"
+
+	// "encoding/base64"
 	"github.com/getgauge/gauge/execution/event"
 	"github.com/getgauge/gauge/execution/result"
 	"github.com/getgauge/gauge/gauge"
 	"github.com/getgauge/gauge/gauge_messages"
 	"github.com/getgauge/gauge/plugin"
 	"github.com/getgauge/gauge/runner"
+	uuid "github.com/satori/go.uuid"
 )
 
 type stepExecutor struct {
@@ -52,12 +64,57 @@ func (e *stepExecutor) executeStep(step *gauge.Step, protoStep *gauge_messages.P
 		executeStepMessage := &gauge_messages.Message{MessageType: gauge_messages.Message_ExecuteStep, ExecuteStepRequest: stepRequest}
 		stepExecutionStatus := e.runner.ExecuteAndGetStatus(executeStepMessage)
 		stepExecutionStatus.Message = append(stepResult.ProtoStepExecResult().GetExecutionResult().Message, stepExecutionStatus.Message...)
-		stepExecutionStatus.Screenshots = append(stepResult.ProtoStepExecResult().GetExecutionResult().Screenshots, stepExecutionStatus.Screenshots...)
+		// stepExecutionStatus.Screenshots = append(stepResult.ProtoStepExecResult().GetExecutionResult().Screenshots, stepExecutionStatus.Screenshots...)
+		logger.Infof(true, "stepExecutionStatus %d Screenshots, \n\n", len(stepExecutionStatus.Screenshots))
+		for _, screenshot := range stepExecutionStatus.Screenshots {
+			logger.Info(true, "Creating screenshot \n\n")
+			bytesReader := bytes.NewReader(screenshot)
+			logger.Info(true, "Creating bytes.NewReader \n\n")
+			image, err := png.Decode(bytesReader)
+			logger.Info(true, "Creating png.Decode \n\n")
+			if err != nil {
+				logger.Errorf(true, "Unable to create image Error : %s\n", err.Error())
+			}
+			os.MkdirAll(filepath.Join(config.ProjectRoot, "reports", "html-report", "screenshots"), 0755)
+			file, err := os.OpenFile(filepath.Join(config.ProjectRoot, "reports", "html-report", "screenshots", fmt.Sprintf("%s.png", uuid.NewV4().String())), os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				logger.Errorf(true, "Unable to create image Error : %s\n", err.Error())
+			}
+			logger.Infof(true, "Creating png.Encode %s \n\n", file.Name())
+			err = png.Encode(file, image)
+			if err == nil {
+				logger.Infof(true, "Saving screenshot at : %s", file.Name())
+				stepExecutionStatus.ScreenshotFiles = append(stepExecutionStatus.ScreenshotFiles, file.Name())
+				file.Close()
+			}
+		}
 		if stepExecutionStatus.GetFailed() {
 			e.currentExecutionInfo.CurrentStep.ErrorMessage = stepExecutionStatus.GetErrorMessage()
 			e.currentExecutionInfo.CurrentStep.StackTrace = stepExecutionStatus.GetStackTrace()
 			setStepFailure(e.currentExecutionInfo)
 			stepResult.SetStepFailure()
+		}
+		if stepExecutionStatus.FailureScreenshot != nil {
+			bytesReader := bytes.NewReader(stepExecutionStatus.FailureScreenshot)
+			image, err := png.Decode(bytesReader)
+			logger.Info(true, "Creating png.Decode \n\n")
+			if err != nil {
+				logger.Errorf(true, "Unable to create image Error : %s\n", err.Error())
+			}
+			os.MkdirAll(filepath.Join(config.ProjectRoot, "reports", "html-report", "failure_screenshots"), 0755)
+			file, err := os.OpenFile(filepath.Join(config.ProjectRoot, "reports", "html-report", "failure_screenshots", fmt.Sprintf("%s.png", uuid.NewV4().String())), os.O_CREATE|os.O_WRONLY, 0644)
+			if err != nil {
+				logger.Errorf(true, "Unable to create image Error : %s\n", err.Error())
+			}
+			logger.Infof(true, "Creating png.Encode %s \n\n", file.Name())
+			err = png.Encode(file, image)
+			if err == nil {
+				logger.Infof(true, "Saving screenshot at : %s", file.Name())
+				file.Close()
+			}
+			stepExecutionStatus.FailureScreenshot = []byte{}
+			stepExecutionStatus.ScreenShot = []byte{}
+			stepExecutionStatus.FailureScreenshotFile = file.Name()
 		}
 		stepResult.SetProtoExecResult(stepExecutionStatus)
 	}
